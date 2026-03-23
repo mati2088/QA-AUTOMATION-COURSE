8af387da501b5267cec2e302571e624431f13a33
nicoagustingz <nicoagustingz@gmail.com>
2026-03-22 22:57:51 -0300

    Simplificate code in dashboardValidation.spec.ts

--- src/tests/dasboardValidation.spec.ts
+++ src/tests/dasboardValidation.spec.ts
@@ -1,49 +1,25 @@
 import { test } from "@playwright/test";
 import { LoginSteps } from "../steps/loginSteps";
 import { DashboardValidationSteps } from "../steps/dashboardValidationSteps";
+import loginData from "../data/loginData.json";
 import "dotenv/config";
 
 const url = process.env.URL ?? "";
 
-test.describe("Dashboard Validation Tests", () => {
   test.beforeEach(async ({ page }) => {
     const loginSteps = new LoginSteps(page);
     await loginSteps.goToPage(url);
-    await loginSteps.iniciarSesion("demo", "demo123");
-  });
+    const {usuarioValido, passwordValida} = loginData.loginExitoso;
+    await loginSteps.iniciarSesion(usuarioValido, passwordValida);
+});
 
-  test("Validar nombre de usuario Juan Perez", async ({ page }) => {
+  test("Validar Datos Dashboard", async ({ page }) => {
     const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarNombreUsuario();
-  });
-
-  test("Validar que Cuenta Corriente es visible", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarCuentaCorriente();
-  });
-
-  test("Validar que Caja de Ahorro es visible", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarCajaDeAhorro();
-  });
-
-  test("Validar que Tarjeta de Crédito es visible", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarTarjetaCredito();
-  });
-
-  test("Validar Saldo disponible en Cuenta Corriente", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarSaldoCuentaCorriente();
-  });
-
-  test("Validar Saldo disponible en Caja de Ahorro", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarSaldoCajaDeAhorro();
-  });
-
-  test("Validar Disponible en Tarjeta de Crédito", async ({ page }) => {
-    const dashboardSteps = new DashboardValidationSteps(page);
     await dashboardSteps.validarSaldoTarjetaCredito();
   });
-});
